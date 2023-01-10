d574c53912ff00e828dca93545f00786f6a5fd70
SumeyraAltinisik <sfa2023@gmail.com>
2023-01-10 16:02:08 +0300

    Add project files.

---
+++ Degiskenler/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string sehir, ilce;
+            //sehir = "İstanbul";
+            //ilce = "Beykoz";
+            //Console.Write(sehir + "-" + ilce);
+            //Console.Read();
+
+            //int sayi;
+            //sayi = 24;
+            //Console.Write(sayi);
+
+            int sayi1, sayi2, toplam;
+            sayi1 = 12;
+            sayi2 = 25;
+            toplam = sayi1 + sayi2;
+            Console.Write(toplam);
+
+            Console.Read();
+
+        }
+    }
+}
