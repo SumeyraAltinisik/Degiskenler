68ddf25300e07d449d9977fb3cfdbf41e6151994
SumeyraAltinisik <sfa2023@gmail.com>
2023-01-10 22:49:23 +0300

    hesap makinesi

--- Degiskenler/Program.cs
+++ Degiskenler/Program.cs
@@ -14,12 +14,28 @@
             //sayi = 24;
             //Console.Write(sayi);
 
-            int sayi1, sayi2, toplam;
-            sayi1 = 12;
-            sayi2 = 25;
-            toplam = sayi1 + sayi2;
-            Console.Write(toplam);
+            //int sayi1, sayi2, toplam;
+            //sayi1 = 12;
+            //sayi2 = 25;
+            //toplam = sayi1 + sayi2;
+            //Console.Write(toplam);
 
+            int sayi1, sayi2, toplam, fark, carpim, bolum;
+            Console.WriteLine("Bir sayı giriniz:");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Birinci sayı:" + sayi1);
+            Console.WriteLine("Bir sayı daha giriniz:");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("İkinci sayı:" + sayi2);
+            
+            toplam = sayi1 + sayi2;
+            Console.WriteLine(toplam);
+            fark = sayi1 - sayi2;
+            Console.WriteLine(fark);
+            carpim = sayi1 * sayi2;
+            Console.WriteLine(carpim);
+            bolum = sayi1 / sayi2;
+            Console.WriteLine(bolum);
             Console.Read();
 
         }
